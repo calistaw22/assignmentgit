13d39844cda42503d11b5b7547d6c6c2940e54c4
calistaw22 <91386468+calistaw22@users.noreply.github.com>
2021-09-25 12:11:07 -0500

    Add files via upload

---
+++ hello_git.py
@@ -0,0 +1,24 @@
+Last login: Thu Sep 23 23:03:53 on ttys000
+Calistas-MacBook-Air:~ calistawhitmore$ python
+Python 2.7.16 (default, Mar 25 2021, 18:52:10) 
+[GCC 4.2.1 Compatible Apple LLVM 10.0.1 (clang-1001.0.37.14)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("hello git world!")
+hello git world!
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
